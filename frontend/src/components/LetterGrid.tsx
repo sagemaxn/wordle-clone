81e9e4b52e87b524bb8c764821dbe0eedf5e84de
sagemaxn <sagemaxn@gmail.com>
2022-02-09 22:46:39 -0500

    modified:   frontend/src/components/LetterGrid.tsx

--- frontend/src/components/LetterGrid.tsx
+++ frontend/src/components/LetterGrid.tsx
@@ -1,42 +1,64 @@
-import React, { useState } from 'react'
-import {Box, Button} from '@chakra-ui/react'
+import React, { useState, useEffect } from "react";
+import { Box, Button } from "@chakra-ui/react";
 
-
-const Square = (a) => <Box>{a}</Box>
-
+const Square = ({letter}) => <Box width="20px">{letter}</Box>;
 
 const LetterGrid = () => {
-    const [ar, setAr] = useState(['s','a','d'])
+  const [ar, setAr] = useState([['','','','',''], ['','','','','']]);
+  const [ind, setInd] = useState(0)
 
-    function push(){
-        let newAr = [...ar]
-        if (newAr.length >=5){
-            return
+  useEffect(() => {
+    console.log(ar);
+  }, [ar]);
+  useEffect(() => {
+      console.log(ind)
+      console.log(ind <=5)
+  }, [ind])
+  useEffect(() => {
+    document.body.addEventListener("keydown", (e) => {
+  //      console.log(e)
+        if (e.key === "Enter" && ind <= 5) {
+               let newInd = ind + 1
+               setInd(newInd)
+               console.log(ind <= 5)
         }
-        newAr.push('s')
-        setAr(newAr)
-        console.log(ar)
-    }
-    function pop(){
-        let newAr = [...ar]
-        newAr.pop()
-        setAr(newAr)
-        console.log(ar)
-    }
+      else if (e.key === "Backspace") {
+        pop();
+      } else{
+        push(e.key, ind);
+      }
+    });
+    return () => {
+      document.body.removeEventListener("keydown", (e) => {
+        console.log(3);
+      });
+    };
+  }, []);
 
-    function gen(){
-       // return ar.map((a) => Square(a))
-       return ar
-    }
-    
-    return (
-    <>{ar}
-    <Button onClick={() => pop()}>pop</Button>
-    <Button onClick={() => push()}>push</Button>
+  function push(keyPress, rowI) {
+    let newAr = [...ar];
+    let row = newAr[rowI]
+    row.push(keyPress);
+    setAr([...newAr]);
+    //  console.log(ar)
+  }
+  function pop() {
+    let newAr = [...ar];
+    newAr.pop();
+    setAr(newAr);
+    //   console.log(ar)
+  }
 
+  function gen() {
+  return ar.map((row) => <div>{row.map(v => <Square letter={v}/>)}</div>)
+    return ar;
+  }
+
+  return (
+    <>
+      {gen()}
     </>
-    
-    )
-}
+  );
+};
 
-export default LetterGrid
+export default LetterGrid;
